cf4e73fe51b049293d9491738587b902f6b5e1a2
pravin <thombalpravin5751@gmail.com>
2025-06-12 09:45:18 +0530

    Mini Project on Bill Calculations using NodeJs and Ejs

---
+++ index.js
@@ -0,0 +1,19 @@
+let express=require("express");
+let body=require("body-parser");
+let app=express();
+app.set("view engine","ejs")
+
+app.use(express.static ("public"))
+app.use(body.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.render("cal.ejs",{calBill:0})
+})
+app.post("/save",(req,res)=>{
+    let {qty,rate}=req.body 
+    let b=qty*rate;
+    res.render("cal.ejs",{calBill:b});
+})
+app.listen(3000,()=>{
+    console.log("server started.....");
+})
